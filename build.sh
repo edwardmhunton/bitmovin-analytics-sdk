594972a8b45978a80502455314105dc2373c4dd4
Daniel Hölbling-Inzko <daniel.hoelbling-inzko@bitmovin.com>
2017-01-20 15:58:09 +0100

    Added build url to script

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ echo "Building image eu.gcr.io/bitmovin-api/bitmovin-analytics-sdk:`git describe
 docker build -t eu.gcr.io/bitmovin-api/bitmovin-analytics-sdk:`git describe` .
 echo "Pushing to Dockerhub: bitmovin-analytics-sdk:$(git describe)"
 gcloud docker -- push "eu.gcr.io/bitmovin-api/bitmovin-analytics-sdk:$(git describe)"
+echo "Image pull url: eu.gcr.io/bitmovin-api/bitmovin-analytics-sdk:$(git describe)"
