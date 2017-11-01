829e1cd43b28c721a2ff7fde4cede7f85256a6a4
Clemens Helm <clemens.helm@gmail.com>
2017-11-01 20:15:20 +0100

    Improve styling of date selection.

--- example-dashboards/comparison/src/components/DateRangeSelection.js
+++ example-dashboards/comparison/src/components/DateRangeSelection.js
@@ -1,34 +1,35 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
-import { Button, OverlayTrigger, Popover, FormGroup, ControlLabel } from 'react-bootstrap';
+import { Button, ToggleButton, ToggleButtonGroup, OverlayTrigger, Popover, FormGroup, ControlLabel }
+  from 'react-bootstrap';
 import moment from 'moment';
 import 'react-datepicker/dist/react-datepicker.css';
 import './DateRangeSelection.css';
 
 const dateRanges = Object.freeze([
   Object.freeze({
-    label: 'Last 24 hours',
+    label: 'Today',
     fromDate: () => moment().startOf('day').toDate(),
     toDate: () => moment().endOf('day').toDate(),
   }),
   Object.freeze({
     label: 'Last 3 days',
-    fromDate: () => moment().startOf('day').subtract(3, 'days').toDate(),
+    fromDate: () => moment().startOf('day').subtract(2, 'days').toDate(),
     toDate: () => moment().endOf('day').toDate(),
   }),
   Object.freeze({
     label: 'Last 7 days',
-    fromDate: () => moment().startOf('day').subtract(7, 'days').toDate(),
+    fromDate: () => moment().startOf('day').subtract(6, 'days').toDate(),
     toDate: () => moment().endOf('day').toDate(),
   }),
   Object.freeze({
     label: 'Last 14 days',
-    fromDate: () => moment().startOf('day').subtract(14, 'days').toDate(),
+    fromDate: () => moment().startOf('day').subtract(13, 'days').toDate(),
     toDate: () => moment().endOf('day').toDate(),
   }),
   Object.freeze({
     label: 'Last 30 days',
-    fromDate: () => moment().startOf('day').subtract(30, 'days').toDate(),
+    fromDate: () => moment().startOf('day').subtract(29, 'days').toDate(),
     toDate: () => moment().endOf('day').toDate(),
   }),
 ]);
@@ -37,55 +38,70 @@ export const initialDateRange = dateRanges[2];
 
 export default class DateRangeSelection extends Component {
   state = {
-    label: initialDateRange.label,
+    currentLabel: initialDateRange.label,
   }
 
   handleDateChange = (attr) => (dateMoment) => {
     const { fromDate, toDate, onChange } = this.props;
     const update = { [attr]: dateMoment.toDate() };
     onChange({ fromDate, toDate, ...update });
-    this.setState({ label: `${moment(fromDate).format('L')} – ${moment(toDate).format('L')}`})
+    this.setState({ currentLabel: `${moment(fromDate).format('L')} – ${moment(toDate).format('L')}`})
   }
 
-  selectRange = (label) => () => {
+  selectRange = (label) => {
     const range = dateRanges.find(r => r.label === label);
     this.props.onChange({ fromDate: range.fromDate(), toDate: range.toDate() });
-    this.setState({ label: range.label });
+    this.setState({ currentLabel: range.label });
   }
 
   render() {
     const { fromDate, toDate } = this.props;
-    const { label } = this.state;
+    const [fromMoment, toMoment] = [fromDate, toDate].map(d => moment(d));
+    const { currentLabel } = this.state;
 
     const dateRangePopover = (
       <Popover id="DateRangeSelection-popover">
         <div className="DateRangeSelection-dateSelection">
-          <FormGroup controlId="fromDate">
+          <FormGroup controlId="DateRangeSelection-fromDate">
             <ControlLabel>From</ControlLabel>
             <DatePicker
-              selected={moment(fromDate)}
+              selected={fromMoment}
+              startDate={fromMoment}
+              endDate={toMoment}
+              selectsStart
               onChange={this.handleDateChange('fromDate')}
+              id="DateRangeSelection-fromDate"
             />
           </FormGroup>
-          <FormGroup controlId="toDate">
+          <FormGroup controlId="DateRangeSelection-toDate">
             <ControlLabel>To</ControlLabel>
             <DatePicker
-              selected={moment(toDate)}
+              selected={toMoment}
+              startDate={fromMoment}
+              endDate={toMoment}
+              selectsStart
               onChange={this.handleDateChange('toDate')}
+              id="DateRangeSelection-toDate"
             />
           </FormGroup>
         </div>
-        <div>
+        <ToggleButtonGroup
+          bsSize="small"
+          type="radio"
+          name="DateRangeSelection-intervalButtons"
+          value={currentLabel}
+          onChange={this.selectRange}
+        >
           {dateRanges.map(({ label }) =>
-            <Button key={label} onClick={this.selectRange(label)}>{label}</Button>)}
-        </div>
+            <ToggleButton key={label} value={label}>{label}</ToggleButton>)}
+        </ToggleButtonGroup>
       </Popover>
     );
 
     return (
       <div>
-        <OverlayTrigger trigger="click" placement="bottom" overlay={dateRangePopover}>
-          <Button>{label}</Button>
+        <OverlayTrigger trigger="click" rootClose placement="bottom" overlay={dateRangePopover}>
+          <Button>{currentLabel}</Button>
         </OverlayTrigger>
       </div>
     );
