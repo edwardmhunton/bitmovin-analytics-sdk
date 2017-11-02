3f5818b3580803dbd43916483bbfbd2c7fa2ef0d
Clemens Helm <clemenshelm@Clemenss-MacBook-Pro.local>
2017-11-02 14:32:02 +0100

    Stored current license key in localStorage.

--- example-dashboards/comparison/src/components/Main.js
+++ example-dashboards/comparison/src/components/Main.js
@@ -6,14 +6,15 @@ import './Main.css';
 
 export default class Main extends Component {
   state = {
-    currentLicenseId: this.props.licenses[0].id,
     fromDate: initialDateRange.fromDate(),
     toDate: initialDateRange.toDate(),
   };
 
+  currentLicenseKey = () => localStorage.getItem('licenseKey') || this.props.licenses[0].licenseKey;
+
   handleLicenseChange = (event) => {
-    const currentLicenseId = event.currentTarget.value;
-    this.setState({ currentLicenseId });
+    localStorage.setItem('licenseKey', event.currentTarget.value)
+    this.forceUpdate();
   }
 
   handleDateRangeChange = ({ fromDate, toDate }) =>
@@ -21,10 +22,8 @@ export default class Main extends Component {
 
   render() {
     const { licenses, apiKey } = this.props;
-    console.log(licenses);
-    const { fromDate, toDate, currentLicenseId } = this.state;
-    console.log(currentLicenseId);
-    const { licenseKey } = licenses.find(l => l.id === currentLicenseId);
+    const { fromDate, toDate } = this.state;
+    const currentLicenseKey = this.currentLicenseKey();
 
     return (
       <Panel className="Main-container">
@@ -33,16 +32,21 @@ export default class Main extends Component {
             <h1>Compare</h1>
             <FormGroup controlId="formControlsSelect" bsSize="small">
               <ControlLabel>License</ControlLabel>
-              <FormControl componentClass="select" placeholder="select" value={currentLicenseId} onChange={this.handleLicenseChange}>
-                {licenses.map(({ id, name, licenseKey }) =>
-                  <option value={id} key={id}>
+              <FormControl
+                componentClass="select"
+                placeholder="select"
+                value={currentLicenseKey}
+                onChange={this.handleLicenseChange}
+              >
+                {licenses.map(({ name, licenseKey }) =>
+                  <option value={licenseKey} key={licenseKey}>
                     {name || licenseKey}
                   </option>)}
               </FormControl>
             </FormGroup>
           </div>
           <DateRangeSelection fromDate={fromDate} toDate={toDate} onChange={this.handleDateRangeChange}/>
-          <ComparisonTable fromDate={fromDate} toDate={toDate} apiKey={apiKey} licenseKey={licenseKey} />
+          <ComparisonTable fromDate={fromDate} toDate={toDate} apiKey={apiKey} licenseKey={currentLicenseKey} />
         </form>
       </Panel>
     );
