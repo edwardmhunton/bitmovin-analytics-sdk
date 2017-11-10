ba0cdace9740a80e55cae955631be44a878dfc21
Clemens Helm <clemens.helm@gmail.com>
2017-11-10 23:56:30 +0100

    Avoid adding a filter twice.

--- example-dashboards/comparison/src/components/Filters.js
+++ example-dashboards/comparison/src/components/Filters.js
@@ -3,18 +3,26 @@ import { DropdownButton, MenuItem } from 'react-bootstrap';
 import Filter from './Filter.js'
 import './Filters.css';
 
+const filterItems = [
+  { name: 'CDN_PROVIDER', label: 'CDN Provider' },
+  { name: 'BROWSER', label: 'Browser' },
+  { name: 'COUNTRY', label: 'Country' },
+  { name: 'PLAYER', label: 'Player' },
+];
+
 export default function Filters ({ onAdd, onUpdate, filters, queryBuilder, licenseKey, fromDate, toDate }) {
   const updateFilter = (name) => (value) => onUpdate({ name, value });
+  const existingFilterNames = filters.map(f => f.name);
+  const unusedFilterItems = filterItems.filter(i => !existingFilterNames.includes(i.name));
+  const noMoreFilters = unusedFilterItems.length === 0;
 
   return (
     <div className="Filters">
       <h2>Filters</h2>
       <div className="Filters-list">
-        <DropdownButton title={'Add a filter'} onSelect={onAdd} id="addFilter">
-          <MenuItem eventKey="CDN_PROVIDER">CDN Provider</MenuItem>
-          <MenuItem eventKey="BROWSER">Browser</MenuItem>
-          <MenuItem eventKey="COUNTRY">Country</MenuItem>
-          <MenuItem eventKey="PLAYER">Player</MenuItem>
+        <DropdownButton title={'Add a filter'} onSelect={onAdd} id="addFilter" disabled={noMoreFilters}>
+          {unusedFilterItems.map(({ name, label }) =>
+            <MenuItem key={name} eventKey={name}>{label}</MenuItem>)}
         </DropdownButton>
         {filters.map(filter =>
           <Filter {...filter}
